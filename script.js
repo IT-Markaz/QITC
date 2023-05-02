ee1bc5b06f0c92accc41ab04a2b620832f18c43e
Bunyod <bunyodbaratov5808@gmail.com>
2023-05-02 18:12:44 +0500

    Animatsiya

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 const Xbtn = document.getElementById('btn');
 const Obtn = document.getElementById('btn2');
-const nav =  document.querySelector('nav');
 const header =  document.querySelector('.header-about');
 Obtn.addEventListener('click', ()=>{
-    Obtn.style.display = 'none';
-    Xbtn.style.display = 'block';
-    nav.style.display = 'block'  
-    header.style.display = 'none'
+    const nav =  document.querySelector('nav');
+    Obtn.style.display = "none"
+    nav.classList.add('animation');
+    nav.classList.remove('hidden')
+    setInterval(()=>{
+        nav.classList.remove('animation');
+    }, 2001)
 })
 Xbtn.addEventListener('click', ()=>{
-    Xbtn.style.display = 'none';
-    Obtn.style.display = 'block';
-    nav.style.display = 'none'
-    header.style.display = 'block'
+    const nav =  document.querySelector('nav');
+    nav.classList.add('hidden');
+    Obtn.style.display = 'inline-block';
 })
